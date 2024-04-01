c48316a0558596c4fe0f9323ce7678b83a73bf0c
Irene Mwova <irene.mwova@student.moringaschool.com>
2024-04-01 13:00:32 +0300

    adds arithmetic function

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
-// Write your code here
+let num1 = 2;
+let num2 = 31
+let num3 = 14;
+let num4 = 5;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+//console.log(Math.ceil(Math.random() * 10));
+let mod = num3 % num4;
+let max = Math.max(11, 12, 13, 19, 20);
+console.log(multiply);
+console.log(mod);
+console.log(random);
+console.log(max);
